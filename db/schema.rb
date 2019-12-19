910c646697a4c3ae54e81c96491af33e205ef5af
tjtanjin <cjtanjin@gmail.com>
2019-12-19 07:19:33 +0000

    Switched from sqlite3 to postgresql.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_09_211251) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "job_name"
