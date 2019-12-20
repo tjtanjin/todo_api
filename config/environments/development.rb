f1e85b8e8b6902c53baba12977aa2d77c0374ebc
tjtanjin <cjtanjin@gmail.com>
2019-12-20 21:37:02 +0800

    Updated to support sending email to new users.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,11 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV["API_DOMAIN"] }
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
