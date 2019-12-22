95226bb38ce9a2d6b3f3c5bda67733b21ef8e00d
tjtanjin <cjtanjin@gmail.com>
2019-12-22 15:44:57 +0800

    Refactored to supply additional information on user role and logged in status.

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -7,7 +7,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    { auth_token: JsonWebToken.encode(user_id: user.id), isAdmin: user.role === "admin", isLoggedIn: true } if user
   end
 
   private
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationController < ApplicationController
    command = AuthenticateUser.call(params[:email], params[:password])
 
    if command.success?
-     render json: { auth_token: command.result }
+     render json: command.result
    else
      render json: { error: command.errors }, status: :unauthorized
    end
