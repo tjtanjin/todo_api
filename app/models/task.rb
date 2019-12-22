13f55f81e0c03ea813c23ae74acc8f70278344d9
tjtanjin <cjtanjin@gmail.com>
2019-12-22 22:15:46 +0800

    Unique task name validation reduced from global level to user level.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :job_name, presence: true, uniqueness: true
+  validates :job_name, presence: true, uniqueness: {scope: :user_id}
   validates :job_desc, :category, presence: true
 
 end
