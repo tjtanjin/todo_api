3d36392163c8f977238109dbdd210bae7aa59711
tjtanjin <cjtanjin@gmail.com>
2019-12-29 17:20:50 +0800

    Added more password requirements.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :task, dependent: :delete_all
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :name, :password, presence: true
+  validate :password_requirements_are_met
 
   def can_modify_user?(user_id)
     role == 'admin' || id.to_s == user_id.to_s
@@ -13,4 +14,18 @@ class User < ApplicationRecord
     role == 'admin'
   end
 
+  def password_requirements_are_met
+    rules = {
+      " must contain at least one lowercase letter"  => /[a-z]+/,
+      " must contain at least one uppercase letter"  => /[A-Z]+/,
+      " must contain at least one digit"             => /\d+/,
+      " must contain at least one special character" => /[^A-Za-z0-9]+/
+    }
+
+    if password != nil
+      rules.each do |message, regex|
+        errors.add( :password, message ) unless password.match( regex )
+      end
+    end
+  end
 end
