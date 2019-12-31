79c112412ed8e8d0644de5534e7ac09666ebb24d
tjtanjin <cjtanjin@gmail.com>
2020-01-01 03:55:58 +0800

    Added validation to prevent deadline being set to the past.

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,11 @@ class Task < ApplicationRecord
   validates :task_name, presence: true, uniqueness: {scope: :user_id}
   validates :task_description, :category, presence: true
   validates_inclusion_of :priority, :in => %w( Low Medium High )
+  validate :deadline_cannot_be_in_the_past
+
+  def deadline_cannot_be_in_the_past
+    if deadline.present? && deadline < Date.today
+      errors.add(:deadline, "can't be in the past")
+    end
+  end 
 end
