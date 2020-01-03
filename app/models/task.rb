ad1e2a382c2e8bd7505e18ccf21b08ee1216d658
tjtanjin <cjtanjin@gmail.com>
2020-01-03 20:49:33 +0800

    Fixed a bug where users cannot reuse task name after task is completed.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  validates :task_name, presence: true, uniqueness: {scope: :user_id}
-  validates :task_description, :category, :deadline, presence: true
+  validates :task_name, :task_description, :category, :deadline, presence: true
+  validates_uniqueness_of :task_name, scope: :user_id, conditions: -> { where.not(priority: ['Completed']) }
   validates_inclusion_of :priority, :in => %w( Low Medium High Completed )
   validate :deadline_cannot_be_in_the_past
 
